09fb7411a136bfea977aa63a621255a463950039
karananu <karanrana3095@gmail.com>
2023-05-04 01:18:25 +0530

    discord

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:{
+    Whithey:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"]
+  },
+}
+
